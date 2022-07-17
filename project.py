4428830032b565b63afe4e92f6d24186169c3505
Sri Harsha <mallaharsha66@gmail.com>
2022-07-17 22:18:10 +0530

    select option and explanation

--- project.py
+++ project.py
@@ -1,3 +1,5 @@
+import os
+from matplotlib import image
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as pl
@@ -7,21 +9,37 @@ import streamlit as st
 
 
 start= '2010-01-01'
-end= '2021-12-31'
+end= '2022-6-30'
 
-st.title("Stock Trend Prediction")
+st.title("Stock Mate")
+def header(url):
+     st.header(f'<p style="color:#0066cc;color:#33ff33;font-size:24px;border-radius:2%;">{url}</p>', unsafe_allow_html=True)
+st.markdown(" Stockmate implement machine learning approach to predict stock prices. Machine learning is effectively implemented in forecasting stock prices. The objective is to predict the stock prices in order to make more informed and accurate investment decisions. We propose a stock price prediction system that integrates mathematical functions, machine learning, and other external factors for the purpose of achieving better stock prediction accuracy and issuing profitable trades.")
+input = st.selectbox(
+     'Enter or select Stock Tiker ',
+     ('AAPL','MSFT','GOOG','GOOGL','AMZN','TSLA','UNH','JNJ','META','NVDA'))
+
+st.write('You selected:', input)
 
 
-input=st.text_input("enter stock ticker",'AAPL')
+# input=st.text_input("enter stock ticker",'AAPL')
 df=data.DataReader(input,'yahoo',start,end)
 
-st.subheader("Data from 2010-2022")
+st.subheader("Data from 2010-2021")
 st.write(df.describe())
+with st.expander("See explanation"):
+     st.write("""
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+     """)
 
 st.subheader("closing price vs time chart")
 fig =pl.figure(figsize=(12,6))
 pl.plot(df.Close)
 st.pyplot(fig)
+with st.expander("See explanation"):
+     st.write("""
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+     """)
 
 st.subheader("closing price vs time chart with 100MA and 200MA")
 ma100=df.Close.rolling(100).mean()
@@ -31,6 +49,10 @@ pl.plot(ma100, 'r')
 pl.plot (ma200 ,'g')
 pl.plot(df.Close, 'b')
 st.pyplot(fig)
+with st.expander("See explanation"):
+     st.write("""
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+     """)
 
 data_training= pd.DataFrame(df['Close'][0:int(len(df)*0.70)])
 data_testing= pd.DataFrame(df['Close'][int(len(df)*0.70):int(len(df))])
@@ -71,6 +93,10 @@ fig2=pl.figure(figsize=(12,6))
 pl.plot(y_test,'b')
 pl.plot(y_predicted)
 st.pyplot(fig2)
+with st.expander("See explanation"):
+     st.write("""
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+     """)
 
 
 
